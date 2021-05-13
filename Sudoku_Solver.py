f6cb0b9b308b75d4f10b71023a03048c2de83482
ShaneBeuerman <shanebeuerman@gmail.com>
2021-05-13 14:28:22 -0600

    Finished Sudoku_Solver
    The Sudoku Solver is finished. It takes in  an unfinished sudoku text file and prints out the possible solutions.

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,16 +1,56 @@
 #Sudoku solver
 
-
-def printBoard(arr):
+def printBoard(array):
     for i in range(9):
-        print(arr[i])
+        print(array[i])
 
-def solveSudoku(arr):
-    #To do
-    print("To do soon")
+def printSolutions():
+    global solutions
+    for solution in solutions:
+        for i in range(9):
+            print(solution[i])
 
-board = [0 for i in range(9)]
-f = open("sudokuboard.txt", "r")
+def horizontalVerify(x, n):
+    global arr
+    for i in range(9):
+        if arr[x][i] == n:
+            return False
+    return True
+
+def verticalVerify(y, n):
+    global arr
+    for i in range(9):
+        if arr[i][y] == n:
+            return False
+    return True
+
+def boxVerify(x, y, n):
+    global arr
+    xBox = (x//3) * 3
+    yBox = (y//3) * 3
+    for i in range(0,3):
+        for j in range(0,3):
+            if arr[xBox+i][yBox+i] == n:
+                return False
+    return True
+
+def solveSudoku():
+    global arr
+    for i in range(9):
+        for j in range(9):
+            if arr[i][j] == 0:
+                for n in range(1,10):
+                    if horizontalVerify(i, n) and verticalVerify(j, n) and boxVerify(i, j, n):
+                        arr[i][j] = n
+                        solveSudoku()
+                        arr[i][j] = 0
+                return
+    t = tuple(map(tuple, arr))
+    solutions.add(t)
+                
+arr = [ 0 for i in range(9)]
+solutions = set()
+f = open("unsolved.txt", 'r')
 lineCount = 0
 while(True):
     line = f.readline()
@@ -19,7 +59,10 @@ while(True):
     if lineCount > 9:
         break
     line = line.strip("\n")
-    line = list(line.split(" "))
-    board[lineCount] = line
+    line = list(map(int, line.split(" ")))
+    arr[lineCount] = line
     lineCount += 1
-printBoard(board)
+printBoard(arr)
+solveSudoku()
+print()
+printSolutions()
